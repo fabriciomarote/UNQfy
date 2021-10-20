903a20d53a04b213348702e50fd692706d1ed600
enadialopez <enadialopez@gmail.com>
2021-10-20 08:43:33 -0300

    se agrega comportamiento en la api

--- UNQfy/api.js
+++ UNQfy/api.js
@@ -5,6 +5,8 @@ const albums = express();
 const tracks = express();
 const unqfy = require('./unqfy');
 
+let port = process.env.PORT || 8080
+
 artists.route('/artists')
 .get((req, res) => {
 
@@ -16,28 +18,42 @@ artists.route('/artists/:artistId')
 .get((req, res) => {
     const artistId = req.params.artistId;
     const artist = unqfy.getArtistById(artistId);
-    res.json(artist);
+    res.status(200).json(artist);
 })
 .delete((req, res) => {
+    const artistId = req.params.artistId;
+    const artist = unqfy.getArtistById(artistId);
+    unqfy.deleteArtist(artist);
+    res.status(204);co
+     
 })
 .patch((req, res) => {
 });
 
 
 albums.route('/albums')
-.get((req, res) => {
-
-})
 .post((req, res) => {
 
 })
 
+albums.route('/albums/search')
+.get((req, res) => {
+    const albumName =  req.query;
+    
+})
+
 albums.route('/albums/:albumId')
 .get((req, res) => {
+    const albumId = req.params.albumId;
+    const album = unqfy.getAlbumById(albumId);
+    res.status(200).json(album);
 
 })
 .delete((req, res) => {
-
+    const albumId = req.params.albumId;
+    const album = unqfy.getAlbumById(albumId);
+    unqfy.deleteAlbum(album);
+    res.status(204)
 })
 .patch((req, res) => {
 
