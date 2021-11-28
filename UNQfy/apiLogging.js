c295153147626f24b8efeedd0218dd3b762d25d7
enadialopez <enadialopez@gmail.com>
2021-11-28 00:06:08 -0300

    se modifica el servicio logging

--- UNQfy/apiLogging.js
+++ UNQfy/apiLogging.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const Logging = require('./logging');
+const fs = require('fs');
 
 const port = process.env.PORT || 4000;
 const app = express();
+const logs = express();
+app.use('/api', logs);
 
 function getLogging() {
     const logging = new Logging();
@@ -11,17 +14,74 @@ function getLogging() {
 
 const { errorHandler, InvalidURLError} = require('./errors'); 
 const logging = getLogging();
+let isActive = true;
 
 app.use(express.json());
-
-
 
 app.listen(port,
     () => console.log(`Puerto ${port} Ok`)
 );
 
+/*app.route("./active"){
+    const body = {  state: req.body.state }
+    .post((req, res) => {
+        logging.setState(body.state);
+    });
+});
 
+**/
+function sendLog(type, msg){
+    winston.add(new Loggly({
+    token: "f066c974-c1da-43ed-9073-c116dcaea9e5",
+    subdomain: "enadialopez",
+    tags: ["Winston-NodeJS"],
+    json: true
+    }));
+    winston.log(type, msg);
+}
 
+function saveLog(message, type) {
+    let contenido = message + ":" + type;
+    fs.appendFileSync('archivo.txt', contenido); //cambiar este nombre
+}
+
+function setState(state){
+    isActive = state;
+}
+
+logs.route('/active')
+.get((req, res) => {
+    if (!isActive){
+        isActive = !isActive;
+    }
+});
+
+logs.route('/desactive')
+.get((req, res) => {
+    if (isActive){
+        isActive = !isActive;
+    }
+
+});
+
+logs.route('/log')
+.post((req, res) => {
+    console.log("pasa por aca 1");
+    if (isActive) {
+        console.log(req.body);
+        const body = {
+                        message: req.body.message, 
+                        type: req.body.type
+                    };
+        sendLog(body.type, body.message);
+        saveLog(body.message, body.type);
+        console.log("pasa por aca 3");
+        res.status(200).res.json({});
+    }
+    else{
+        console.log('Could not send messege to Loggly becouse is unactive');
+    }
+});
 
 app.use('*', function(req, res) {
     const invalidError = new InvalidURLError();
--- UNQfy/logging.js
+++ UNQfy/logging.js
@@ -8,37 +8,19 @@ const {Loggly} = require('winston-loggly-bulk');
 
 class Logging {
 
-    sendLoggin(type, msg){
-        winston.add(new Loggly({
-        token: "f066c974-c1da-43ed-9073-c116dcaea9e5",
-        subdomain: "enadialopez",
-        tags: ["Winston-NodeJS"],
-        json: true
-        }));
-        winston.log(type, msg);
+    sendNotify(message, type) {
+        return fetch('http://localhost:3000/api/notify',{
+            method: 'POST',
+            body: JSON.stringify({
+                message: message, 
+                type: type
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            } 
+        });
     }
-
-    sendNotify(msg, type) {
-        this.sendLoggin(type, msg);
-        //saveLogArchive(msg, type);
-    }
-
-/*
-function saveLogArchive(msj, type) {
-    let content = msj + type;
-    readFile('archivo', data)
-    .then((data) =>{
-        //const parseData = JSON.parse(data);
-        //console.log(parseData);
-
-        writeFile('logging.text', content)
-        .then(() => console.log('Archivo escrito correctamente'));
-    })    
-    .catch((error) => {
-        console.log('Ocurrio un error');
-    });
-}
-*/
+    
 }
 
 module.exports = Logging;
